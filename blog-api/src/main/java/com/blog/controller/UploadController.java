e5bcb17e4baab85210415e66f1069904f7daee15
Zhang <3484868205@qq.com>
2023-02-17 17:36:43 +0800

    Controller of UPload

---
+++ blog-api/src/main/java/com/blog/controller/UploadController.java
@@ -0,0 +1,39 @@
+package com.blog.controller;
+
+import com.blog.utils.QiniuUtils;
+import com.blog.vo.Result;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.UUID;
+
+/**
+ * @Author: 34848
+ * @Date: 2023/02/15/19:46
+ * @Description:
+ */
+@RestController
+@RequestMapping("/upload")
+public class UploadController {
+    @Autowired
+    private QiniuUtils qiniuUtils;
+
+    @PostMapping
+    public Result upload(@RequestParam("image")MultipartFile file){
+        String originalFilename = file.getOriginalFilename();
+        String fileName = UUID.randomUUID() + "." + StringUtils.substringAfter(originalFilename, ".");
+
+        boolean upload = qiniuUtils.upload(file, fileName);
+        if(upload){
+            return Result.success(QiniuUtils.url+fileName);
+        }
+        return Result.fail(20001,"文件上传失败");
+
+    }
+}
