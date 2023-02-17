848eedbb345dc3d9d6c0789fc5f5a208696c10c7
Zhang <3484868205@qq.com>
2023-02-17 20:28:03 +0800

    privacy set null

--- blog-api/src/main/java/com/blog/utils/InfoUtils.java
+++ blog-api/src/main/java/com/blog/utils/InfoUtils.java
@@ -3,5 +3,5 @@ package com.blog.utils;
 //储存数据
 public class InfoUtils {
     //密码盐
-    public static final String salt="zjw1314";
+    public static final String salt="";
 }
--- blog-api/src/main/java/com/blog/utils/QiniuUtils.java
+++ blog-api/src/main/java/com/blog/utils/QiniuUtils.java
@@ -15,7 +15,8 @@ import org.springframework.web.multipart.MultipartFile;
 @Component
 public class QiniuUtils {
 
-    public static  final String url = "http://rq4ed4tzy.hb-bkt.clouddn.com/";
+    //你的图床
+    public static  final String url = "";
 
     @Value("${qiniu.accessKey}")
     private  String accessKey;
@@ -29,7 +30,7 @@ public class QiniuUtils {
         //...其他参数参考类注释
         UploadManager uploadManager = new UploadManager(cfg);
         //...生成上传凭证，然后准备上传
-        String bucket = "mszlu--blog";
+        String bucket = "";
         //默认不指定key的情况下，以文件内容的hash值作为文件名
         try {
             byte[] uploadBytes = file.getBytes();
