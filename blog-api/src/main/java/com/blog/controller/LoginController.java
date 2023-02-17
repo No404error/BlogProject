31b1d422dbbcb6c16ee2d6fb4e7a69c2913a317c
Zhang <3484868205@qq.com>
2023-02-17 17:14:43 +0800

    Controller of Login

--- blog-api/src/main/java/com/blog/controller/LoginController.java
+++ blog-api/src/main/java/com/blog/controller/LoginController.java
@@ -9,12 +9,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * @Author: 34848
- * @Date: 2023/01/14/9:59
- * @Description:
- */
-
 @RestController
 @RequestMapping("/login")
 public class LoginController {
