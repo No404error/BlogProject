fb67602216bc6a34832b6743eb5e6bc02bb46669
Zhang <3484868205@qq.com>
2023-02-17 18:24:49 +0800

    check wrong

--- blog-api/src/main/java/com/blog/dao/mapper/ArticleBodyMapper.java
+++ blog-api/src/main/java/com/blog/dao/mapper/ArticleBodyMapper.java
@@ -4,11 +4,6 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.blog.dao.pojo.ArticleBody;
 import org.apache.ibatis.annotations.Mapper;
 
-/**
- * @Author: 34848
- * @Date: 2023/01/15/15:19
- * @Description:
- */
 @Mapper
 public interface ArticleBodyMapper extends BaseMapper<ArticleBody> {
 }
